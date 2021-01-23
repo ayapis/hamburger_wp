db9dd35c788857def1fe1cfdab94f8c875e301aa
ayapis <ayapisss@gmail.com>
2021-01-24 08:45:36 +0900

    Googlefontの読み込み

--- functions.php
+++ functions.php
@@ -11,5 +11,6 @@
       wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.7.0' );
       wp_enqueue_style( 'hamburger', get_template_directory_uri() . '/css/style.css', array(), '1.0.0' );
       wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
+      wp_enqueue_style( 'google-webfont-style', '//fonts.googleapis.com/css?family=Lato:wght@400;700&' );
     }
     add_action( 'wp_enqueue_scripts', 'hamburger_script' );
