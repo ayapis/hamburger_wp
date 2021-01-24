945d69ceaac2965530645aab9e65bf97d891e21d
ayapis <ayapisss@gmail.com>
2021-01-24 09:31:58 +0900

    ヘッダー内のサイトタイトルのタグをページごとで可変に

--- header.php
+++ header.php
@@ -14,7 +14,12 @@
     <div class="l-content__main">
       <header class="l-header">
         <div class="l-header__wrap">
+          <?php
+          if(is_front_page()){ ?>
           <h1 class="l-header__title"><a href="#"><?php bloginfo( 'name' ); ?></a></h1>
+          <?php }else{ ?>
+          <p class="l-header__title"><a href="#"><?php bloginfo( 'name' ); ?></a></p>
+          <?php } ?>
           <?php get_search_form(); ?>
         </div>
       </header>
